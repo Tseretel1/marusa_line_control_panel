34b5185921b2e95082ee67c62abee5897a366829
George <119735003+Tseretel1@users.noreply.github.com>
2026-03-11 23:21:08 +0400

    fixed order details visuals

--- marusa_line_control_panel/src/http.interceptor.ts
+++ marusa_line_control_panel/src/http.interceptor.ts
@@ -1,8 +1,14 @@
-import { HttpInterceptorFn } from '@angular/common/http';
-
+import { HttpErrorResponse, HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { AuthService } from './app/shared/services/auth.service';
 export const httpInterceptor: HttpInterceptorFn = (request, next) => {
   const token = localStorage.getItem("token");
-
+  const user = localStorage.getItem("user");
+  const authService = inject(AuthService);
+  function redirectToLogin(): void {
+    authService.forceLogout();
+  }
   if (!request.url.includes('https://api.cloudinary.com')) {
     if (token) {
       request = request.clone({
@@ -11,6 +17,14 @@ export const httpInterceptor: HttpInterceptorFn = (request, next) => {
         }
       });
     }
+    return next(request).pipe(
+    catchError((error: HttpErrorResponse) => {
+      if (error.status === 401) {
+        redirectToLogin();
+      }
+      return throwError(() => error);
+    })
+  );
   } 
   else {
   }
